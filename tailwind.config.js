19881c26caa408363180b67dcb862dbca1f5776a
Shradha Saxena <103318017+ShradhaSaxena23@users.noreply.github.com>
2024-03-07 21:38:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors: {
+        'Greyish': '#B6BDC4',
+        'Whitish':'#FBFCFD',
+        'Gray':'#E1E4E6'
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
