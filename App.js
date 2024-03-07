372d69bac2f9b8020263efc5f77e42b8f3935e1b
Shradha Saxena <103318017+ShradhaSaxena23@users.noreply.github.com>
2024-03-07 19:53:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Body from "./component/Body";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Body/>
+
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
